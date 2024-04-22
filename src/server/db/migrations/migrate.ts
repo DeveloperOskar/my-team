5d622a18417ebaf7b68c7a0158703883fbedf79f
Oskar Eriksson <dev.oskar.eriksson@gmail.com>
2024-04-22 10:41:33 +0200

    now using neon serverless db

--- src/server/db/migrations/migrate.ts
+++ src/server/db/migrations/migrate.ts
@@ -1,7 +1,5 @@
 import "dotenv/config";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { db, conn } from "../index";
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { db } from "../index";
 // This will run migrations on the database, skipping the ones already applied
 await migrate(db, { migrationsFolder: "src/server/db/migrations" });
-// Don't forget to close the connection, otherwise the script will hang
-await conn.end();
